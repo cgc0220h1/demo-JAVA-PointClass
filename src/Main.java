9d92d787fae3e4de5aa54b4808cbadde340d71ea
vanduc2514 <vanduc2514@gmail.com>
2020-03-28 12:35:36 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //Test Point2D class
+        Point2D point2D = new Point2D();
+        System.out.println(point2D);
+        point2D = new Point2D(1.5f,2.5f);
+        System.out.println(point2D);
+        System.out.println();
+
+        //Test Point3D class
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+        point3D = new Point3D(1.5f,2.5f,3.5f);
+        System.out.println(point3D);
+        System.out.println();
+    }
+}
